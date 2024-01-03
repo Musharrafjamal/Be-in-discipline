c38bef921a5142c74f27391865f4a35776cdb589
Musharraf Jamal <musharrafjamal08@gmail.com>
2024-01-03 14:47:25 +0530

    update cors url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://beindescipline-api.vercel.app"],
+    origin: ["https://beindescipline.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
