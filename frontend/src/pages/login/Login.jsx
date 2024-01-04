8a04f4980122f6715f62814991d410b37f899133
Musharraf Jamal <musharrafjamal08@gmail.com>
2024-01-04 12:24:41 +0530

    update login-page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { redirect } from "react-router-dom";
 import "./login.css";
 import Nav from "../../components/nav/Nav";
 import img from "../../assets/img/login-svg.svg";
@@ -15,17 +14,19 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("https://beindescipline-server.onrender.com/auth/login", {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        "https://beindescipline-server.onrender.com/auth/login",
+        {
+          username,
+          password,
+        }
+      );
       const token = response.data.token;
       localStorage.setItem("token", token);
       localStorage.setItem("username", username);
       localStorage.setItem("profilePic", response.data.profilePic);
 
-      // navigate("/app");
-      redirect("/app")
+      await navigate("/app");
     } catch (err) {
       console.log("Login falid", err.message);
       setLoginFailed(true);
@@ -37,10 +38,13 @@ const Login = () => {
       <Nav />
       <div className="login-container">
         <div className="login-left-wrapper">
-          <form className="login-form"onSubmit={(e) => {
-          e.preventDefault();
-          handleLogin();
-        }}>
+          <form
+            className="login-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleLogin();
+            }}
+          >
             <h2>Login</h2>
             <input
               type="text"
@@ -56,17 +60,17 @@ const Login = () => {
               name="password"
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button>Login</button>
+            <button type="submit">Login</button>
+            {loginFailed && (
+              <p
+                style={{
+                  color: "red",
+                }}
+              >
+                Login falid!
+              </p>
+            )}
           </form>
-          {loginFailed && (
-            <p
-              style={{
-                color: "red",
-              }}
-            >
-              Login falid!
-            </p>
-          )}
         </div>
         <div className="login-img-wrapper">
           <img src={img} alt="" />
